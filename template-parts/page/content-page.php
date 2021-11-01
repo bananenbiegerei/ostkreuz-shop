487843bb84a9bd54c0ef5e7c7d0ba376b1602461
Ingo Schmid <ingo@bananenbiegerei.de>
2021-11-01 14:13:50 +0100

    everything for woo

--- template-parts/page/content-page.php
+++ template-parts/page/content-page.php
@@ -1,8 +1,16 @@
+<header class="text-center grid-container">
+	<div class="grid-x grid-margin-x align-center padding-top-6 padding-bottom-6">
+		<div class="cell medium-12">
+			<h1><?php the_title(); ?></h1>
+		</div>
+	</div>
+	<hr class="margin-bottom-2">
+</header>
 <div class="grid-container">
-	<div class="grid-x grid-margin-x">
-		<div class="medium-12 cell">
-			<?php the_title(); ?>
-			<?php the_content(); ?>
+	<div class="grid-x grid-margin-x align-center">
+			<div class="cell medium-12">
+				<?php the_content(); ?>
+			</div>
 		</div>
 	</div>
 </div>
